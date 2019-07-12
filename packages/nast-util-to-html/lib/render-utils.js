0edef676460fc8449c48406a55b4a2dc22173d2a
dragonman225 <zxcvb22217@gmail.com>
2019-07-13 02:59:43 +0800

    Merge pull request #1 from Scya597/master
    modify renderBookmark function

--- packages/nast-util-to-html/lib/render-utils.js
+++ packages/nast-util-to-html/lib/render-utils.js
@@ -169,28 +169,46 @@ function renderBookmark(node) {
   let iconImg = iconURL ? `<img src="${iconURL}" style="width: 16px; height: 16px; min-width: 16px; margin-right: 4px;">` : ''
   let coverImg = coverURL ? `<img src="${coverURL}" style="display: block; object-fit: cover; border-radius: 1px; width: 100%; height: 100%;">` : ''
 
+  // let html = `\
+  // <div class="bookmark">
+  //   <div style="display: flex;">
+  //     <div style="display: flex; flex-wrap: wrap-reverse; align-items: stretch; text-align: left; overflow: hidden; border: 1px solid rgba(55, 53, 47, 0.16); border-radius: 3px; position: relative; flex-grow: 1; color: rgb(55, 53, 47);">
+  //       <div style="flex: 4 1 180px; min-height: 60px; overflow: hidden; text-align: left;">
+  //         <a href="${link}" target="_blank" rel="noopener noreferrer" style="display: block; color: inherit; text-decoration: none; height: 100%;">
+  //           <div style="cursor: pointer; user-select: none; transition: background 120ms ease-in 0s; width: 100%; display: block; padding: 14px; height: 100%;">
+  //             <div style="font-size: 14px; line-height: 20px; max-height: 20px; overflow: hidden;">${title}</div>
+  //             <div style="font-size: 12px; line-height: 16px; color: rgba(55, 53, 47, 0.6); max-height: 32px; overflow: hidden;">${description}</div>
+  //             <div style="font-size: 12px; line-height: 16px; display: flex; overflow: hidden; margin-top: 4px;">
+  //               ${iconImg}
+  //               <div style="min-width: 0px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${link}</div>
+  //             </div>
+  //           </div>
+  //         </a>
+  //       </div>
+  //       <div style="flex: 1 1 180px; min-height: 80px; display: block; position: relative;">
+  //         <div style="position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px;">
+  //           <div style="width: 100%; height: 100%;">
+  //             ${coverImg}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+  // `
   let html = `\
   <div class="bookmark" style="margin: 8px 0;">
     <div style="display: flex;">
-      <div style="display: flex; flex-wrap: wrap-reverse; align-items: stretch; text-align: left; overflow: hidden; border: 1px solid rgba(55, 53, 47, 0.16); border-radius: 3px; position: relative; flex-grow: 1; color: rgb(55, 53, 47);">
-        <div style="flex: 4 1 180px; min-height: 60px; overflow: hidden; text-align: left;">
-          <a href="${link}" target="_blank" rel="noopener noreferrer" style="display: block; color: inherit; text-decoration: none; height: 100%;">
-            <div style="cursor: pointer; user-select: none; transition: background 120ms ease-in 0s; width: 100%; display: block; padding: 14px; height: 100%;">
-              <div style="font-size: 14px; line-height: 20px; max-height: 20px; overflow: hidden;">${title}</div>
-              <div style="font-size: 12px; line-height: 16px; color: rgba(55, 53, 47, 0.6); max-height: 32px; overflow: hidden;">${description}</div>
-              <div style="font-size: 12px; line-height: 16px; display: flex; overflow: hidden; margin-top: 4px;">
-                ${iconImg}
+      <div style="display: flex; text-align: left; overflow: hidden; border: 1px solid rgba(55, 53, 47, 0.16); border-radius: 0.7rem; position: relative; flex-grow: 1; color: rgb(55, 53, 47);">
+        <div style="min-height: 1rem; overflow: hidden; text-align: left;">
+          <a href="${link}" target="_blank" rel="noopener noreferrer" style="display: block; color: inherit; text-decoration: none;">
+            <div style="cursor: pointer; user-select: none; transition: background 120ms ease-in 0s; width: 100%; display: block; padding: 1rem;">
+              <div style="font-size: 14px; overflow: hidden;">${title}</div>
+              <div style="font-size: 12px; display: flex; overflow: hidden;">
                 <div style="min-width: 0px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${link}</div>
               </div>
             </div>
           </a>
-        </div>
-        <div style="flex: 1 1 180px; min-height: 80px; display: block; position: relative;">
-          <div style="position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px;">
-            <div style="width: 100%; height: 100%;">
-              ${coverImg}
-            </div>
-          </div>
         </div>
       </div>
     </div>
