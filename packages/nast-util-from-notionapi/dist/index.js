69a3aaca2f4751463a08f47423493ce30e884de5
dragonman225 <zxcvb22217@gmail.com>
2019-07-16 00:36:51 +0800

    sparkles: Properly support bulleted_list and numbered_list.

--- packages/nast-util-from-notionapi/dist/index.js
+++ packages/nast-util-from-notionapi/dist/index.js
@@ -71,20 +71,86 @@ function makeTree(allRecords) {
         /** The slower way. */
         let childrenIDs = node.raw_value.content;
         if (childrenIDs != null) {
+            let pseudoBlock;
+            /**
+             * FSM with 3 states: normal, bulleted, numbered.
+             * Total 3^2 = 9 cases.
+             */
+            let state = 'normal';
             for (let j = 0; j < childrenIDs.length; ++j) {
                 let indexOfChildReference = map[childrenIDs[j]];
                 let childReference = list[indexOfChildReference];
                 /**
-                 * Notion's bug: When downloading a public page as a "reader",
-                 * some blocks are not accessible (Notion responses {"role": "none"}).
+                 * Notion's bug: When downloading a public page,
+                 * some blocks are not accessible (There is an ID in "content" array,
+                 * but we can't find a block having that ID) even if the page is set to "public".
                  */
                 if (childReference != null) {
-                    node.children.push(childReference);
+                    // TODO: Too much duplicate code. Need to clean-up.
+                    switch (state) {
+                        case 'normal': {
+                            if (childReference.type === 'bulleted_list') {
+                                pseudoBlock = newPseudoBlock('unordered_list');
+                                pseudoBlock.children.push(childReference);
+                                node.children.push(pseudoBlock);
+                                state = 'bulleted';
+                            }
+                            else if (childReference.type === 'numbered_list') {
+                                pseudoBlock = newPseudoBlock('ordered_list');
+                                pseudoBlock.children.push(childReference);
+                                node.children.push(pseudoBlock);
+                                state = 'numbered';
+                            }
+                            else {
+                                node.children.push(childReference);
+                            }
+                            break;
+                        }
+                        case 'bulleted': {
+                            if (childReference.type === 'bulleted_list') {
+                                pseudoBlock.children.push(childReference);
+                            }
+                            else if (childReference.type === 'numbered_list') {
+                                pseudoBlock = newPseudoBlock('ordered_list');
+                                pseudoBlock.children.push(childReference);
+                                node.children.push(pseudoBlock);
+                                state = 'numbered';
+                            }
+                            else {
+                                node.children.push(childReference);
+                                state = 'normal';
+                            }
+                            break;
+                        }
+                        case 'numbered': {
+                            if (childReference.type === 'numbered_list') {
+                                pseudoBlock.children.push(childReference);
+                            }
+                            else if (childReference.type === 'bulleted_list') {
+                                pseudoBlock = newPseudoBlock('unordered_list');
+                                pseudoBlock.children.push(childReference);
+                                node.children.push(pseudoBlock);
+                                state = 'bulleted';
+                            }
+                            else {
+                                node.children.push(childReference);
+                                state = 'normal';
+                            }
+                            break;
+                        }
+                        default:
+                    }
                 }
             }
         }
     }
     return treeRoot;
+}
+function newPseudoBlock(type) {
+    return {
+        type,
+        children: []
+    };
 }
 module.exports = async function downloadPageAsTree(pageID, agent) {
     assert_1.default(typeof pageID === 'string');
@@ -109,6 +175,10 @@ module.exports = async function downloadPageAsTree(pageID, agent) {
     async function getChildrenRecords(ids) {
         let requests = makeRecordRequests(ids);
         let response = await api.getRecordValues(requests);
+        if (response.statusCode !== 200) {
+            console.log(response);
+            throw new Error('Fail to get data.');
+        }
         let responseData = response.data;
         let childrenRecords;
         let childrenIDs;
