7a2d831fd5a92fb25409e0f075b279014f5fba19
dragonman225 <zxcvb22217@gmail.com>
2019-10-19 16:48:46 +0800

    fix: Handle http url icons in callout block

--- packages/nast-util-to-html/src/widgets/callout.ts
+++ packages/nast-util-to-html/src/widgets/callout.ts
@@ -6,9 +6,12 @@ import { renderBlock, renderTitle } from '../render-utils'
 function renderCallout(
   node: Nast.Callout
 ): string {
+  let iconHTML = /^http/.test(node.icon || '')
+    ? `<img src="${node.icon}" style="height: 1.5em;">` : node.icon
+
   let content = `\
-<div>
-  ${node.icon ? node.icon : ''}
+<div style="padding-top: 2.5px;">
+  ${iconHTML}
 </div>
 <div style="margin-left: 8px;">
   ${renderTitle(node.text, false, '')}
