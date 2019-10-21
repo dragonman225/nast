adf6173f3303dc9320cd51f7113f2f8c5cf3428b
dragonman225 <zxcvb22217@gmail.com>
2019-10-22 00:57:02 +0800

    refactor: Re-implement bookmark widget
    The old one copied from Notion is bloated and not easy to customize.
    The new one solves above problems.

--- packages/nast-util-to-html/dist/widgets/bookmark.js
+++ packages/nast-util-to-html/dist/widgets/bookmark.js
@@ -3,23 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const render_utils_1 = require("../render-utils");
 /** !!! This should be replaced with more efficient implementation. */
 function renderBookmark(node) {
-    let title = node.title ? node.title : '';
-    let link = node.link;
+    let titleHTML = node.title ? `<h5>${node.title}</h5>` : '';
+    let descHTML = node.description ? `<p>${node.description}</p>` : '';
+    let linkHTML = `<p>${node.link}</p>`;
     let content = `\
-<div style="display: flex;">
-  <div style="display: flex; text-align: left; overflow: hidden; border: 1px solid rgba(55, 53, 47, 0.16); border-radius: 0.7rem; position: relative; flex-grow: 1; color: rgb(55, 53, 47);">
-    <div style="min-height: 1rem; overflow: hidden; text-align: left; width: 100%;">
-      <a href="${link}" target="_blank" rel="noopener noreferrer" style="display: block; color: inherit; text-decoration: none;">
-        <div style="cursor: pointer; user-select: none; transition: background 120ms ease-in 0s; width: 100%; display: block; padding: 16px;">
-          <div style="font-size: 14px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${title}</div>
-          <div style="font-size: 12px; display: flex; overflow: hidden; padding-top: 3px;">
-            <div style="font-size: 12px; min-width: 0px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${link}</div>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
-</div>`;
+<a href="${node.link}">
+  ${titleHTML}
+  ${descHTML}
+  ${linkHTML}
+</a>`;
     return render_utils_1.renderBlock(node, content);
 }
 exports.default = renderBookmark;
