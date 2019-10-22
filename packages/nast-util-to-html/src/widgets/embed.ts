b278fbc4f80105300f98eaa9084cbce3b4384cd1
dragonman225 <zxcvb22217@gmail.com>
2019-10-23 01:33:26 +0800

    misc: Update embed widget
    Replace deprecated "frameborder" attr of <iframe> with CSS border

--- packages/nast-util-to-html/src/widgets/embed.ts
+++ packages/nast-util-to-html/src/widgets/embed.ts
@@ -10,12 +10,14 @@ function renderEmbed(
   let aspectRatio = node.aspectRatio * 100
 
   let iframeSandbox = 'allow-scripts allow-popups allow-forms allow-same-origin'
-  let iframeStyle = 'position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; border-radius: 1px; pointer-events: auto; background-color: rgb(247, 246, 245);'
+  let iframeStyle = 'position: absolute; left: 0px; top: 0px; width: 100%;\
+ height: 100%; border: none; border-radius: 1px; pointer-events: auto;\
+ background-color: rgb(247, 246, 245);'
 
   let content = `\
 <div style="width: ${width};">
   <div style="position: relative; min-height: 100px; height: 0; padding-bottom: ${aspectRatio}%;">
-    <iframe src="${source}" frameborder="0" sandbox="${iframeSandbox}" allowfullscreen style="${iframeStyle}"></iframe>
+    <iframe src="${source}" sandbox="${iframeSandbox}" allowfullscreen style="${iframeStyle}"></iframe>
   </div>
 </div>`
 
