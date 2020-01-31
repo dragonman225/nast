5b7d60e9740220de48158aab01eda6a6ed5262e5
dragonman225 <zxcvb22217@gmail.com>
2020-02-01 01:36:39 +0800

    react-renderer: Improve Icon styling interface

--- packages/nast-util-to-react/src/components/Icon.tsx
+++ packages/nast-util-to-react/src/components/Icon.tsx
@@ -2,11 +2,24 @@ import * as React from "react"
 
 export interface IconProps {
   src: string
+  size?: string
+  borderRadius?: string
 }
 
 export function Icon(props: IconProps) {
   const isEmoji = !/^http/.test(props.src)
-  return (
-    isEmoji ? <>{props.src}</> : <img src={props.src} />
-  )
+  const defaultSize = "1em"
+  const defaultBorderRadius = "3px"
+  const style: React.CSSProperties = isEmoji ?
+    { width: props.size || defaultSize, textAlign: "center" } :
+    {
+      width: props.size || defaultSize,
+      height: props.size || defaultSize,
+      borderRadius: props.borderRadius || defaultBorderRadius
+    }
+  if (isEmoji) {
+    return <div style={style}>{props.src}</div>
+  } else {
+    return <img src={props.src} style={style} />
+  }
 }
