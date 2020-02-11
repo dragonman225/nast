3910e89a05a823962bdd10d9c519fc320aba7009
dragonman225 <zxcvb22217@gmail.com>
2020-02-11 14:25:34 +0800

    react-renderer: Change icon of Page

--- packages/nast-util-to-react/src/components/Page.tsx
+++ packages/nast-util-to-react/src/components/Page.tsx
@@ -4,6 +4,11 @@ import { BlockRendererProps } from "../interfaces"
 import { Icon } from "./Icon"
 import { SemanticStringArray } from "./SemanticString"
 
+//const iconPageEmpty =
+//  <path d="M16,1H4v28h22V11L16,1z M23.172,11H16V3.828L23.172,11z M24,27H6V3h8v10h10V27z" />
+const iconPageNonEmpty =
+  <path d="M16,1H4v28h22V11L16,1z M16,3.828L23.172,11H16V3.828z M24,27H6V3h8v10h10V27z M8,17h14v-2H8V17z M8,21h14v-2H8V21z M8,25h14v-2H8V25z" />
+
 export interface PageProps extends BlockRendererProps {
   current: NAST.Page
 }
@@ -34,15 +39,13 @@ export function Page(props: PageProps) {
                   fill: "inherit",
                   opacity: "0.8"
                 }}>
-                  <g>
-                    <path d="M16,1H4v28h22V11L16,1z M23.172,11H16V3.828L23.172,11z M24,27H6V3h8v10h10V27z"></path>
-                  </g>
+                  <g>{iconPageNonEmpty}</g>
                 </svg>
             }
           </div>
           <div className={`${blockName}__Title`}>
-            <SemanticStringArray
-              semanticStringArray={data.title.length ? data.title : [["Untitled"]]} />
+            <SemanticStringArray semanticStringArray={data.title.length ?
+              data.title : [["Untitled"]]} />
           </div>
         </div>
       </a>
