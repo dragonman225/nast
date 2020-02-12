41122a349b9efca20cdb91452dd0def890721b3c
dragonman225 <zxcvb22217@gmail.com>
2020-02-12 12:50:00 +0800

    react-renderer: Add hover-to-show anchor to Heading

--- packages/nast-util-to-react/src/components/Heading.tsx
+++ packages/nast-util-to-react/src/components/Heading.tsx
@@ -10,13 +10,21 @@ export interface HeadingProps extends BlockRendererProps {
 export function Heading(props: HeadingProps) {
   const blockName = "Heading"
   const data = props.current
+  const content =
+    <>
+      <a href={`#${data.uri}`}>
+        <svg width="16" height="16" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
+        </svg>
+      </a>
+      <SemanticStringArray semanticStringArray={data.title} />
+    </>
   switch (data.depth) {
     case 1: {
       return (
         <>
           <h1 id={data.uri} className={`${blockName} ${blockName}--D1`}>
-            <SemanticStringArray
-              semanticStringArray={data.title} />
+            {content}
           </h1>
           {props.children}
         </>
@@ -27,8 +35,7 @@ export function Heading(props: HeadingProps) {
       return (
         <>
           <h2 id={data.uri} className={`${blockName} ${blockName}--D2`}>
-            <SemanticStringArray
-              semanticStringArray={data.title} />
+            {content}
           </h2>
           {props.children}
         </>
@@ -39,8 +46,7 @@ export function Heading(props: HeadingProps) {
       return (
         <>
           <h3 id={data.uri} className={`${blockName} ${blockName}--D3`}>
-            <SemanticStringArray
-              semanticStringArray={data.title} />
+            {content}
           </h3>
           {props.children}
         </>
