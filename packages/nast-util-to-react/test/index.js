a2fc40cdb7482c50e0dd2a1704ca924b9581a5a9
dragonman225 <zxcvb22217@gmail.com>
2020-02-21 21:20:44 +0800

    react-renderer: Update test script

--- packages/nast-util-to-react/test/index.js
+++ packages/nast-util-to-react/test/index.js
@@ -10,7 +10,15 @@ const html = `\
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.19.0/themes/prism.min.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="theme.css">
+  <style>
+    .PageRoot {
+      width: 900px;
+      max-width: 100%;
+      margin: 0 auto;
+      padding: 0px 96px 30vh;
+    }
+  </style>
 </head>
 <body>${renderToHTML(tree)}</body>
 </html>
