d5d3d36eba689d4438866ab2cb2351d35c351aff
Alexander Wang <zxcvb22217@gmail.com>
2020-05-13 01:05:47 +0800

    notionapi: Add license information to transformTitle

--- packages/nast-util-from-notionapi/src/transformTitle.ts
+++ packages/nast-util-from-notionapi/src/transformTitle.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2019-present Wen-Zhi Wang <zxcvb22217@gmail.com>
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a MIT license that can be found 
+ * in the LICENSE file.
+ */
+
 import * as Notion from "notionapi-agent/dist/interfaces"
 import * as NAST from "nast-types"
 import { convertFileUrl, getBlockUri, getHashLink } from "./util"
