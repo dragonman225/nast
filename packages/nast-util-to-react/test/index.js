27357bf5a4a3cae0424401a5cdddb61bac45ec16
Alexander Wang <zxcvb22217@gmail.com>
2021-04-16 01:49:35 +0800

    refactor(react): Improve Headings CSS

--- packages/nast-util-to-react/test/index.js
+++ packages/nast-util-to-react/test/index.js
@@ -17,6 +17,8 @@ const html = `\
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.19.0/themes/prism.min.css">
   <link rel="stylesheet" type="text/css" href="theme.css">
